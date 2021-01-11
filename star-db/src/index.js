ca9f186402fd531ccd779896a87f447bd5940d24
Shakhban <shakhban777@gmail.com>
2021-01-11 22:05:21 +0300

    fix some bugs and test fetch on swapi

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+const getResource = async (url) => {
+	const res = await fetch(url);
+	const body = await res.json();
+	return body;
+};
+
+getResource('https://swapi.dev/api/people/1/')
+	.then((res) => console.log(res));
