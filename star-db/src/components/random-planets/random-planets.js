7f9bc238e330948e406c0b89ba16c00c77ddd6d5
Shakhban <shakhban777@gmail.com>
2021-01-13 22:50:59 +0300

    Add spinner

--- star-db/src/components/random-planets/random-planets.js
+++ star-db/src/components/random-planets/random-planets.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
 
 import './random-planets.css';
 
@@ -31,29 +32,31 @@ export default class RandomPlanets extends Component {
 
 		const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
 
-		return(
+		return (
+
 			<div className="random-planet jumbotron rounded">
-      		  <img className="planet-image"
-              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-				  alt="planet"/>
-        <div>
-          <h4>{ name }</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{ population }</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{ rotationPeriod }</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{ diameter }</span>
-            </li>
-          </ul>
-        </div>
-      </div>
+				<Spinner />
+				<img className="planet-image"
+					src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+					alt="planet" />
+				<div>
+					<h4>{name}</h4>
+					<ul className="list-group list-group-flush">
+						<li className="list-group-item">
+							<span className="term">Population</span>
+							<span>{population}</span>
+						</li>
+						<li className="list-group-item">
+							<span className="term">Rotation Period</span>
+							<span>{rotationPeriod}</span>
+						</li>
+						<li className="list-group-item">
+							<span className="term">Diameter</span>
+							<span>{diameter}</span>
+						</li>
+					</ul>
+				</div>
+			</div>
 		);
 	}
 };
---
+++ star-db/src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+	return (
+		<div class="loadingio-spinner-double-ring-ivywpugrbaf"><div class="ldio-eet14rbv33">
+			<div></div>
+			<div></div>
+			<div><div></div></div>
+			<div><div></div></div>
+		</div></div>
+	);
+};
+
+export default Spinner;
