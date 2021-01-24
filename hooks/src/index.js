ed16872726b40b47354e827fc3b10d29aef41b0d
Shakhban <shakhban777@gmail.com>
2021-01-24 23:07:56 +0300

    Add useEffect

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,21 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const MyContext = React.createContext();
-
 const App = () => {
-	return (
-		<MyContext.Provider value='Hello world!'>
-			<Child/>
-		</MyContext.Provider>
-	);
+
+	const [ value, setValue ] = useState(0);
+	const [ visible, setVisible ] = useState(true);
+
+	if (visible) {
+		return (
+			<div>
+				<button
+					onClick={() => setValue((s) => s + 1)}>+</button>
+				<button
+					onClick={() => setVisible(false)}>hide</button>
+				<HookCounter value={value}/>
+				<ClassCounter value={value}/>
+			</div>
+		);
+	} else {
+		return <button
+			onClick={() => setVisible(true)}>show</button>
+	}
 };
 
-const Child = () => {
-	const value = useContext(MyContext);
+const HookCounter = ({value}) => {
 
-	return <p>{value}</p>
-};
+	useEffect(() => {
+		console.log('use effect');
+		return () => {console.log('clear')};
+	}, [value])
+
+	return <p> {value} </p>;
+}
+
+class ClassCounter extends Component {
+
+	componentDidMount() {
+		console.log('class: mount');
+	}
+
+	componentDidUpdate(props) {
+		console.log('class: update');
+	}
+
+	componentWillUnmount() {
+		console.log('class: unmount');
+	}
+
+	render() {
+		return <p> {this.props.value} </p>;
+	}
+}
 
 ReactDOM.render(
   <React.StrictMode>
