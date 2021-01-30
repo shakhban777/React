309027c756f2cdc7f536d87db8ef03c733ed200f
Shakhban <shakhban777@gmail.com>
2021-01-31 00:07:11 +0300

    Add createStore, subscribe, dispatch

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const reducer = (state = 0, action) => {
 
 	switch (action.type) {
@@ -8,10 +10,10 @@ const reducer = (state = 0, action) => {
 	}
 };
 
-let state = reducer(undefined, {});
+const store = createStore(reducer);
+store.subscribe(() => {
+	console.log(store.getState());
+});
 
-state = reducer(state, {type: 'INC'});
-console.log(state);
-
-state = reducer(state, {type: 'INC'});
-console.log(state);
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
