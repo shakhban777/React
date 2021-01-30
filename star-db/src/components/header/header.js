480eecc3558b60db86c9af6b0d2989e7626642ea
Shakhban <shakhban777@gmail.com>
2021-01-30 12:13:03 +0300

    litlle fix

--- star-db/src/components/header/header.js
+++ star-db/src/components/header/header.js
@@ -5,22 +5,23 @@ import './header.css';
 
 const Header = ({ onServiceChange }) => {
 	return (
-		<div className="header d-flex">
+		<div className="header d-flex align-items-center">
 			<h3>
 				<Link to="/">Star DB</Link>
 			</h3>
-			<ul className='d-flex'>
+			<ul className='d-flex align-items-center'>
 				<li><Link to='/people/'>People</Link></li>
 				<li><Link to='/planets/'>Planets</Link></li>
 				<li><Link to='/starships/'>Starships</Link></li>
 				<li><Link to='/login'>Login</Link></li>
 				<li><Link to='/secret'>Secret</Link></li>
+				<li><button
+						className='btn btn-sm btn-primary'
+						onClick={ onServiceChange } >
+						Change Service
+					</button>
+				</li>
 			</ul>
-			<button
-				className='btn btn-sm btn-primary'
-				onClick={ onServiceChange } >
-				Change Service
-			</button>
 		</div>
 	);
 };
