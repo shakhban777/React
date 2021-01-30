9c2091b6b395b6476fc4db76b40b225436f837a8
Shakhban <shakhban777@gmail.com>
2021-01-30 23:58:29 +0300

    Create reducer on native JS

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+const reducer = (state = 0, action) => {
+
+	switch (action.type) {
+		case 'INC':
+			return state + 1;
+		default:
+			return state;
+	}
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, {type: 'INC'});
+console.log(state);
+
+state = reducer(state, {type: 'INC'});
+console.log(state);
