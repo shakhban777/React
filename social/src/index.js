343bf58de4c32dd145e3862d95559206594ed4ec
Shakhban <shakhban777@gmail.com>
2021-02-15 20:52:47 +0300

    Refactoring and fix some bugs

--- social/src/index.js
+++ social/src/index.js
@@ -1,4 +1,22 @@
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {addMessage, addPost, subscribe, updateNewMessageText, updateNewPostText} from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+const rerenderEntireTree = (state) => {
+   ReactDOM.render(
+      <React.StrictMode>
+         <App
+            state={state}
+            addPost={addPost}
+            updateNewPostText={updateNewPostText}
+            addMessage={addMessage}
+            updateNewMessageText={updateNewMessageText}/>
+      </React.StrictMode>,
+      document.getElementById('root')
+   );
+};
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
