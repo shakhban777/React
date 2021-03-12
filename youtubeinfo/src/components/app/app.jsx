80e57c2c176504a6c9e6df95ed4b160a3ab3da3a
Shakhban <shakhban777@gmail.com>
2021-03-12 23:11:31 +0300

    Test changes

--- youtubeinfo/src/components/app/app.jsx
+++ youtubeinfo/src/components/app/app.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, {Component} from "react";
+import {Button, Jumbotron, Form} from 'react-bootstrap';
+import './app.scss';
+import Result from "../result/result";
 
-const App = () => {
-   return(
-      <form>
-         <input/>
-      </form>
-   );
-};
+export default class App extends Component {
+   state = {
+      baseURL: 'https://youtube.googleapis.com/youtube/v3/search?',
+      searching: 'shakhban',
+      apiKey: 'AIzaSyDLjNPbLM1TSxhjJqqKZ0N1jrRQxbzqQ-4',
+      maxResults: 10,
+      orderBy: 'viewCount',
+      requireURL:`${this.baseURL}part=snippet&maxResults=${this.maxResults}&order=${this.orderBy}&q=${this.searching}&key=${this.apiKey}`
+   };
 
-export default App;
+   onSearch = (e) => {
+      e.preventDefault();
+      console.log(e);
+   }
+
+   render() {
+      return (
+         <div className='app'>
+            <Jumbotron>
+               <Form className='mb-5'
+                     onSubmit={this.onSearch}>
+                  <Form.Group controlId="formBasicEmail">
+                     <Form.Label>Поиск видео</Form.Label>
+                     <Form.Control type="text" placeholder="Введите название"/>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                     Поиск
+                  </Button>
+               </Form>
+               <Result state={this.state}/>
+            </Jumbotron>
+         </div>
+      );
+   }
+}
