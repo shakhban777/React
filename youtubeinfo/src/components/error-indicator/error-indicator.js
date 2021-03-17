d0eb5a5f31173b7787148a4d2a8be8bd259538ba
Shakhban <shakhban777@gmail.com>
2021-03-17 21:32:44 +0300

    Build project

---
+++ youtubeinfo/src/components/error-indicator/error-indicator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './error-indicator.css';
+import icon from './error.png'
+
+const ErrorIndicator = () => {
+	return(
+		<div className='error-indicator'>
+			<img src={icon} alt="error-icon" />
+			<span className='boom'>BOOM!</span>
+			<span>
+				Something has gone terribly wrong
+			</span>
+			<span>
+				(but we've already send specialists to fix it)
+			</span>
+		</div>
+	);
+};
+
+export default ErrorIndicator;
