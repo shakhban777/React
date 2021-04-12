9f34edbb96d7232b5c1380a6f15f9969e55f0cd1
Shakhban <shakhban777@gmail.com>
2021-04-12 18:29:07 +0300

    Finish project

--- calculator/src/components/app/app.jsx
+++ calculator/src/components/app/app.jsx
@@ -7,38 +7,42 @@ class App extends Component {
    state = {
       result: 0,
       dotPressed: false,
-      firstNumber: null
+      finishedCalculate: false,
+      firstNumber: null,
+      currentOperation: null
    };
 
+   endsWithOperation() {
+      return this.state.result.toString().endsWith('/') || this.state.result.toString().endsWith('*')
+         || this.state.result.toString().endsWith('-') || this.state.result.toString().endsWith('+');
+   }
+
    addNewNumber(num) {
-      this.setState({result: num});
+      this.setState({result: num, finishedCalculate: false});
    }
 
    addNumber(num) {
-      this.setState({result: this.state.result.toString() + num});
+      this.setState({result: this.state.result.toString() + num, finishedCalculate: false});
    }
 
    reverseNum() {
-      if (this.state.result.toString().includes('/' || '*' || '-' || '+')) {
-      } else if (this.state.result.toString().endsWith('.')) {
-         this.setState({dotPressed: false});
-      } else {
-         this.setState({result: -this.state.result});
-      }
+      if (this.state.result.toString().endsWith('.')) this.setState({dotPressed: false});
+      if (!this.state.currentOperation) this.setState({result: -this.state.result});
    }
 
    addDot(num) {
-      if (!this.state.result) {
+      if (!this.state.result || this.state.finishedCalculate) {
          this.setState({result: `0.`, dotPressed: true});
-      } else if (this.state.result.toString().endsWith('/' || '*' || '-' || '+')) {
+      } else if (this.endsWithOperation()) {
          this.setState({result: `${this.state.result}0.`, dotPressed: true});
       } else {
          this.setState({result: this.state.result.toString() + num, dotPressed: true});
       }
+      this.setState({finishedCalculate: false});
    }
 
    getNumber = (num) => {
-      if (!this.state.result && num !== '.' && num !== '±') {
+      if ((!this.state.result || this.state.finishedCalculate) && num !== '.' && num !== '±') {
          this.addNewNumber(num);
       } else if (num === '±' && this.state.result) {
          this.reverseNum();
@@ -50,7 +54,7 @@ class App extends Component {
    }
 
    clean() {
-      this.setState({result: 0, dotPressed: false});
+      this.setState({result: 0, dotPressed: false, finishedCalculate: false, currentOperation: null});
    }
 
    del() {
@@ -60,45 +64,60 @@ class App extends Component {
    }
 
    equal() {
-      if (this.state.result.toString().includes('/')) {
-         const secondNumber = +(this.state.result.split('/')[1]);
-         const divisionResult = parseFloat((+this.state.firstNumber / secondNumber).toFixed(15));
-         this.setState({result: divisionResult});
-      } else if (this.state.result.toString().includes('*')) {
-         const secondNumber = +(this.state.result.split('*')[1]);
-         const multiResult = parseFloat((+this.state.firstNumber * secondNumber).toFixed(15));
-         this.setState({result: multiResult});
-      } else if (this.state.result.toString().includes('+')) {
-         const secondNumber = +(this.state.result.split('+')[1]);
-         const plusResult = parseFloat((+this.state.firstNumber + secondNumber).toFixed(15));
-         this.setState({result: plusResult});
-      } else if (this.state.result.toString().includes('-')) {
-         const secondNumber = +(this.state.result.split('-')[1]);
-         const minusResult = parseFloat((+this.state.firstNumber - secondNumber).toFixed(15));
-         this.setState({result: minusResult});
+      if (this.state.currentOperation) {
+         const secondNumber = +(this.state.result.split(this.state.currentOperation)[1]);
+         // eslint-disable-next-line no-new-func
+         const calc = new Function('a', 'b', `return parseFloat((a ${this.state.currentOperation} b).toFixed(5))`);
+         const res = calc(this.state.firstNumber, secondNumber);
+         this.setState({result: res, finishedCalculate: true, currentOperation: null});
+         return res;
       }
    }
 
    operationSet(operation) {
-      this.setState({firstNumber: this.state.result, dotPressed: false});
-      this.setState({result: this.state.result + operation});
+      if (this.state.currentOperation && !this.state.finishedCalculate && this.endsWithOperation()) {
+         let oldResult = this.state.result;
+         let deleteOperation = oldResult.slice(0, -1);
+         this.setState({
+            result: deleteOperation + operation,
+            dotPressed: false,
+            currentOperation: operation
+         })
+      } else if (this.state.currentOperation && !this.endsWithOperation()) {
+         let res = this.equal();
+
+         this.setState({
+            firstNumber: parseFloat(res),
+            currentOperation: operation,
+            dotPressed: false,
+            result: res + operation,
+            finishedCalculate: false
+         });
+      } else {
+         this.setState({
+            firstNumber: +this.state.result,
+            dotPressed: false,
+            result: this.state.result + operation,
+            currentOperation: operation,
+            finishedCalculate: false
+         });
+      }
    }
 
-   getOperation = (oper) => {
-      if (oper === 'C') {
+   getOperation = (operation) => {
+      if (operation === 'C') {
          this.clean();
-      } else if (oper === '=') {
+      } else if (operation === '=') {
          this.equal();
-      } else if (oper === '<=') {
+      } else if (operation === '<=') {
          this.del();
-      } else if (oper === '/' || oper === '*' || oper === '+' || oper === '-') {
-         this.operationSet(oper);
+      } else if (operation === '/' || operation === '*' || operation === '+' || operation === '-') {
+         this.operationSet(operation);
       }
    }
 
    render() {
       const {result} = this.state;
-
       return (
          <div className='wrapper'>
             <div className='app'>
